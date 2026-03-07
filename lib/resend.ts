20b0e56cb98f061c5c5828830cfa42091dbb4bc7
okewunmi <git config --global user.email>
2026-03-07 11:49:37 +0100

    .evn updting

--- lib/resend.ts
+++ lib/resend.ts
@@ -2,7 +2,7 @@ import { Resend } from "resend";
 
 export const resend = new Resend(process.env.RESEND_API_KEY);
 
-const FROM = `${process.env.RESEND_FROM_NAME || "Da Hausa Initiative"} <${process.env.RESEND_FROM_EMAIL || "newsletter@dahausa.org"}>`;
+const FROM = `${process.env.RESEND_FROM_NAME || "Da Hausa Initiative"} <${process.env.RESEND_FROM_EMAIL || "emails@dahausainitiative.org"}>`;
 
 // Send welcome email to new subscriber
 export async function sendWelcomeEmail(to: string, name?: string) {
