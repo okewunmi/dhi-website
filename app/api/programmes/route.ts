f3d6fcca9b538350b2b2e907162968e64df12e17
okewunmi <git config --global user.email>
2026-08-30 17:37:26 +0100

    feat(api): update GET method to include cache control headers

--- app/api/programmes/route.ts
+++ app/api/programmes/route.ts
@@ -1,8 +1,22 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
+// export const dynamic = "force-dynamic";
+// import { getProgrammes } from "@/lib/supabase-server";
+
+// export async function GET() {
+//   const programmes = await getProgrammes();
+//   return NextResponse.json({ programmes });
+// }
+
 export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
+import { NextResponse } from "next/server";
 import { getProgrammes } from "@/lib/supabase-server";
 
 export async function GET() {
   const programmes = await getProgrammes();
-  return NextResponse.json({ programmes });
+  return NextResponse.json(
+    { programmes },
+    { headers: { "Cache-Control": "no-store, no-cache, must-revalidate" } }
+  );
 }
